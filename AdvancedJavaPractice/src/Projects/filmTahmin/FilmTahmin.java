361885622397715f19d878dc236c2278533a340b
Yunus Pektas <yunuspektas@protonmail.com>
2022-03-28 12:36:42 +0300

    FilmTahmin 03.28.2022

--- AdvancedJavaPractice/src/Projects/filmTahmin/FilmTahmin.java
+++ AdvancedJavaPractice/src/Projects/filmTahmin/FilmTahmin.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class FilmTahmin {
 
-	static List<String> film = new ArrayList<>(Arrays.asList("JOKER","INCEPTION","PIYANIST","GREENMILE","LEON","GODFATHER","JURASSICPARK","TITANIC"));
+    static List<String> film = new ArrayList<>(Arrays.asList("JOKER", "INCEPTION", "PIYANIST", "GREENMILE", "LEON", "GODFATHER", "JURASSICPARK", "TITANIC"));
 /*
  TASK :
   yukarıdaki film listinde bulunan filmlerden herhangi birisini kullanıcının filim ismini  görmeden
@@ -19,14 +19,72 @@ public class FilmTahmin {
   kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  console yazdırınız.
 
  */
-	
-	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("~~~FILM TAHMIN OYUNUNA HOSGELDINIZ~~~");
-		
-		System.out.println("1-"+(film.size())+" arasinda bir sayi giriniz");
+    public static void main(String[] args) {
+// static List<String> film = new ArrayList<>(Arrays.asList("JOKER","INCEPTION","PIYANIST","GREENMILE","LEON","GODFATHER","JURASSICPARK","TITANIC"));
+        Scanner scan = new Scanner(System.in);
+        String selectedFilm = "";
 
-	}
+        System.out.println("~~~FILM TAHMIN OYUNUNA HOSGELDINIZ~~~");
+
+        System.out.print("1-" + (film.size()) + " arasinda bir sayi giriniz : ");
+
+        int wantedFilmNo = scan.nextInt() - 1;
+        System.out.println("Seçtiğiniz filmde " + film.get(wantedFilmNo).length() + " harf var.");
+        for (int i = 0; i < film.get(wantedFilmNo).length(); i++) {
+            System.out.print("_" + " ");
+
+        }
+        System.out.println("");
+        guessNameOfFilm(film.get(wantedFilmNo));
+
+    }
+
+    private static void guessNameOfFilm(String s) {
+        int guessNumber = 2 * s.length(); // maksimum tahmin sayımız
+        //tahmin edilen harfler seçilen filmde varsa, ekrana sadece o harflerin gözükmesi için
+        //yeni bir String türünde array oluşturuldu.
+
+        List<String> arr = new ArrayList<>();
+        // for döngüsü ile doğru tahmin edilen harfler ekrana yazıldı
+        for (int i = 0; i < s.length(); i++) {
+            arr.add(i, "_");
+
+        }
+
+        int lastGuessNumber = 0;
+        boolean guess = true;
+        Scanner scan = new Scanner(System.in);
+        while (guess) {
+            System.out.println("Bir harf Tahmin ediniz : " + "( " + lastGuessNumber + " / " + guessNumber + " )");
+            for (int i = 0; i < arr.size(); i++) {
+                System.out.print(arr.get(i) + " ");
+
+            }
+            System.out.println("");
+            String letter = scan.next().toUpperCase();
+            if (!s.contains(letter)) lastGuessNumber++;
+            else {
+                // for döngüsü ile doğru tahmin edilen harfler yeni oluşturulan Listin ilgili indexine eklendi
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.split("")[i].equals(letter)) {
+                        arr.set(i, letter);
+                    }
+
+                }
+            }
+            if (lastGuessNumber == guessNumber) {
+                System.out.println("Tahmin hakkınız doldu ");
+                System.out.println("Seçtiğiniz film : " + s + " idi");
+                break;
+            }
+            if (!arr.contains("_")) {
+                System.out.println("Tebrikler Bildiniz ");
+                break;
+            }
+
+        }
+
+
+    }
 }
