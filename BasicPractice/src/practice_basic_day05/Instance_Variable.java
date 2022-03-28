00d273dd83119ca528f6ebe0ab2e58e350cdf01b
Yunus Pektas <yunuspektas@protonmail.com>
2022-03-28 15:52:36 +0300

    03.28.2022

--- BasicPractice/src/practice_basic_day05/Instance_Variable.java
+++ BasicPractice/src/practice_basic_day05/Instance_Variable.java
@@ -1,5 +1,7 @@
 package practice_basic_day05;
 
+import java.util.Scanner;
+
 public class Instance_Variable {
     /*
      * - Main method icerisinde bir ogrenciye ait variable’lar olusturun ve bu
@@ -18,4 +20,32 @@ public class Instance_Variable {
      *
      * Not :Instance variable'lar object'lere aittir ve diger adi object variable'dir
      */
+    public static void main(String[] args) {
+        String isim ="Ahmet";
+        String soyisim ="Polatlı";
+        String sinif = "1/A";
+        String okulNo = "12322";
+
+
+        isimDegistir(isim, soyisim, sinif, okulNo);
+        System.out.println(isim + " " + soyisim + " " + sinif + " " + okulNo);
+
+        soyisimdegistir(soyisim);
+        System.out.println(isim + " " + soyisim + " " + sinif + " " + okulNo);
+    }
+
+    private static void soyisimdegistir(String soyisim) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen yeni soyisminizi giriniz : ");
+        soyisim = scan.next();
+    }
+
+    private static void isimDegistir(String isim, String soyisim, String sinif, String okulNo ) {
+        isim = "Mehmet";
+        soyisim = "Alatlı";
+        sinif = "2/B";
+        okulNo = "5543";
+
+        System.out.println(isim + " " + sinif + " " + okulNo);
+    }
 }
