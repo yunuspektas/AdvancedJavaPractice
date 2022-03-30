f092b5f4c36c07a5c351b896445af130481bc2d4
Mirac Taylan <kahtani@protonmail.com>
2022-03-31 01:01:47 +0300

    03.29.2002

--- BasicPractice/src/HW2_ArrayListQuestions/Q2.java
+++ BasicPractice/src/HW2_ArrayListQuestions/Q2.java
@@ -3,6 +3,6 @@ package HW2_ArrayListQuestions;
 public class Q2 {
     public static void main(String[] args) {
         // Creating a list of unique elements taken from the user,
-        // then sorting and printing these elements using an ArrayList in Java.
+        // then sorting and printing these elements using an ArrayList in Java..
     }
 }
